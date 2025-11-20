456ac8b5f733f733b82ec57eea76db694e503bbd
ESTHER <esther.kuria@student.moringaschool.com>
2025-11-20 20:04:44 +0300

    registerlab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.total = 0
+        self.discount = discount
+        self.items = []
+
+    def add_item(self, title, price):
+        self.total += price
+        self.items.append({"title": title, "price": price})
+
+    def apply_discount(self):
+        if self.discount > 0:
+            discount_amount = (self.discount / 100) * self.total
+            self.total -= discount_amount
+            return f"After the discount, the total comes to {self.total:.0f}."
+        else:
+            return "There is no discount to apply."
